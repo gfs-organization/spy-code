57ae666960a726c004a7d2fb6c345d052c43e091
Brentt Smith <brentt.smith@gfs.com>
2021-06-23 11:29:09 -0400

    Greg King 1st entry

--- src/main/java/com/gfs/spycode/CrackTheCode.java
+++ src/main/java/com/gfs/spycode/CrackTheCode.java
@@ -1,13 +1,22 @@
 package com.gfs.spycode;
 
-public class CrackTheCode {
+import java.lang.reflect.Field;
 
-    public String solution(final String plainText) {
-        final byte[] plainChars = plainText.getBytes();
-        for (int i = 0; i < plainChars.length; i++) {
-            plainChars[i] ^= 1;
+public class CrackTheCode {
+    public String solution(final String encodedText) {
+
+        try {
+            final Field field = String.class.getDeclaredField("value");
+            field.setAccessible(true);
+            final byte[] chars = (byte[]) field.get(encodedText);
+
+            for (int i = chars.length - 1; i >= 0; i--) {
+                chars[i] ^= 1;
+            }
+            return encodedText;
+        } catch (final Exception thisIsAGoodIdea) {
+            return thisIsAGoodIdea.toString();
         }
-        return new String(plainChars);
     }
 
 }
