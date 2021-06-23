9a5ad2a96f4ef75e2bb86e28ff2d077b4999940e
Brentt Smith <brentt.smith@gfs.com>
2021-06-23 08:46:04 -0400

    Sameer Khan 1st answer

--- src/main/java/com/gfs/spycode/CrackTheCode.java
+++ src/main/java/com/gfs/spycode/CrackTheCode.java
@@ -2,19 +2,22 @@ package com.gfs.spycode;
 
 public class CrackTheCode {
 
-    public String solution(final String plainText) {
-        final char[] plainChars = plainText.toCharArray();
-        for (int i = 0; i < plainChars.length; i++) {
-            //            System.out.println("Original: " + (plainChars[i] + 0));
-            plainChars[i] ^= 1;
-            //            System.out.println("First: " + (plainChars[i] + 0));
-            //            plainChars[i] -= 64;
-            //            System.out.println("Second: " + (plainChars[i] + 0));
+    public String solution(final String encodedText) {
+        if (encodedText == null || encodedText.isEmpty()) {
+            return "";
         }
-        //        System.out.println("Original: " + plainText);
-        final String flipped = new String(plainChars);
-        //        System.out.println("Flipped: " + flipped);
-        return flipped;
+        char[] decodedChars = new char[encodedText.length()];
+        int counter = 0;
+        for (int c : encodedText.toCharArray()) {
+            if (c % 2 == 0) {
+                decodedChars[counter] = (char) (c + 1);
+            } else {
+                decodedChars[counter] = (char) (c - 1);
+            }
+            counter++;
+        }
+
+        return String.valueOf(decodedChars);
     }
 
 }
