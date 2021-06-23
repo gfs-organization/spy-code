cd3e9b0d275ec90c8170193d661d61092313be6c
Brentt Smith <brentt.smith@gfs.com>
2021-06-23 08:59:20 -0400

    Grant Avery 1st entry

--- src/main/java/com/gfs/spycode/CrackTheCode.java
+++ src/main/java/com/gfs/spycode/CrackTheCode.java
@@ -1,20 +1,21 @@
 package com.gfs.spycode;
 
 public class CrackTheCode {
+    public String solution(final String encodedText) {
+        char[] chars = encodedText.toCharArray();
+        int length = encodedText.length();
+        char[] decodedText = new char[length];
 
-    public String solution(final String plainText) {
-        final char[] plainChars = plainText.toCharArray();
-        for (int i = 0; i < plainChars.length; i++) {
-            //            System.out.println("Original: " + (plainChars[i] + 0));
-            plainChars[i] ^= 1;
-            //            System.out.println("First: " + (plainChars[i] + 0));
-            //            plainChars[i] -= 64;
-            //            System.out.println("Second: " + (plainChars[i] + 0));
+        for (int i = 0; i < chars.length; i++) {
+            int charDecimal = chars[i];
+            // If the char's ascii value is even, go up one, otherwise go down one
+            if (charDecimal % 2 == 0) {
+                decodedText[i] = (char) (charDecimal + 1);
+            } else {
+                decodedText[i] = (char) (charDecimal - 1);
+            }
         }
-        //        System.out.println("Original: " + plainText);
-        final String flipped = new String(plainChars);
-        //        System.out.println("Flipped: " + flipped);
-        return flipped;
-    }
 
+        return String.valueOf(decodedText, 0, length);
+    }
 }
