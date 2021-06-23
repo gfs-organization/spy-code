b1373e6a38695fa54fef6ef07570b57921939d98
Brentt Smith <brentt.smith@gfs.com>
2021-06-23 08:33:08 -0400

    Kyle Niewiada 2nd answer

--- src/main/java/com/gfs/spycode/CrackTheCode.java
+++ src/main/java/com/gfs/spycode/CrackTheCode.java
@@ -1,18 +1,15 @@
 package com.gfs.spycode;
 
-import java.nio.charset.StandardCharsets;
-
 public class CrackTheCode {
 
     public String solution(final String encodedText) {
-        var encodedTextBytes = encodedText.getBytes(StandardCharsets.US_ASCII);
         var results = new byte[encodedText.length()];
 
         for (var i = 0; i < encodedText.length(); i++) {
-            if ((encodedTextBytes[i] % 2 == 0)) {
-                results[i] = encodedTextBytes[i] += 1;
+            if (encodedText.charAt(i) % 2 == 0) {
+                results[i] = (byte) (encodedText.charAt(i) + 1);
             } else {
-                results[i] = encodedTextBytes[i] -= 1;
+                results[i] = (byte) (encodedText.charAt(i) - 1);
             }
         }
         return new String(results);
