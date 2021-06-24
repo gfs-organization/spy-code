520d6f7a8cca2daa30c0fdee62efd632d5ced974
Brentt Smith <brentt.smith@gfs.com>
2021-06-24 16:43:32 -0400

    Wes Bustraan 2nd entry

--- src/main/java/com/gfs/spycode/CrackTheCode.java
+++ src/main/java/com/gfs/spycode/CrackTheCode.java
@@ -2,12 +2,17 @@ package com.gfs.spycode;
 
 public class CrackTheCode {
 
-    public String solution(final String plainText) {
-        final byte[] plainChars = plainText.getBytes();
-        for (int i = 0; i < plainChars.length; i++) {
-            plainChars[i] ^= 1;
-        }
-        return new String(plainChars);
+    public String solution(final String encodedText) {
+
+        final char[] buffer = encodedText.toCharArray();
+
+        int i = buffer.length - 1;
+        do {
+            buffer[i] ^= 1;
+            i--;
+        } while (i >= 0);
+
+        return new String(buffer);
     }
 
 }
