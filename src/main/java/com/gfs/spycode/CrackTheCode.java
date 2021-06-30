579b50c0582d3e4a86b0320a1bbf6dea7f31fc44
Brentt Smith <brentt.smith@gfs.com>
2021-06-29 20:04:53 -0400

    James Uhe 1st entry

--- src/main/java/com/gfs/spycode/CrackTheCode.java
+++ src/main/java/com/gfs/spycode/CrackTheCode.java
@@ -1,13 +1,17 @@
 package com.gfs.spycode;
 
+import java.nio.charset.StandardCharsets;
+
 public class CrackTheCode {
 
-    public String solution(final String plainText) {
-        final byte[] plainChars = plainText.getBytes();
-        for (int i = 0; i < plainChars.length; i++) {
-            plainChars[i] ^= 1;
+    public String solution(final String encodedText) {
+        byte[] b = new byte[encodedText.length()];
+        byte[] b2 = encodedText.getBytes(StandardCharsets.UTF_8);
+        for (int k = 0; k < b.length; k++) {
+            b[k] = (byte) ((int) b2[k] + 1);
+            b[k] -= (byte) ((int) b2[k] % 2 + (int) b2[k] % 2);
         }
-        return new String(plainChars);
+        return new String(b, StandardCharsets.UTF_8);
     }
 
 }
